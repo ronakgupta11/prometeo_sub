bf07c28b014bb61a19c4d9f57b6360be41f85dfe
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-01-21 17:01:02 +0530

    some change

--- hardhat/contracts/IIITJToken.sol
+++ hardhat/contracts/IIITJToken.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+// deployed at 0xb99FCa642Fc5F58eC774eb33DE88D25848B55f60
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
