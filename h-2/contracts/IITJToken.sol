061346515b85f2d8da0cfa2b4c617063a2b6b496
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-01-22 09:30:09 +0530

    contract changes

--- h-2/contracts/IITJToken.sol
+++ h-2/contracts/IITJToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 // 0xb99FCa642Fc5F58eC774eb33DE88D25848B55f60
-
+// https://mumbai.polygonscan.com/address/0xb99FCa642Fc5F58eC774eb33DE88D25848B55f60#code
 contract IITJToken is ERC20, Ownable, ERC20Permit {
     constructor() ERC20("IITJToken", "IITJT") ERC20Permit("IITJToken") {
         _mint(msg.sender, 1000 * 10 ** 18);
